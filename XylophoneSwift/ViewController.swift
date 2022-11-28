f178f73c2021814649a83b26285c94108b307434
Kenneth Ng <kenneth.ng@lifehikes.com>
2022-11-28 18:20:23 +0800

    Play audio and pass params into function

--- XylophoneSwift/ViewController.swift
+++ XylophoneSwift/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("key pressed")
+        
+//        print(sender.titleLabel?.text)
+        
+        playSound(soundName: sender.titleLabel!.text ?? "C")
+        
+    }
+    
+    func playSound(soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
+    
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try! AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    
+    }
 }
-
